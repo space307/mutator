e4e68e477bda0ce818a6d20291fdff7ca1b51447
Aleksandr Ermolaev <a.ermolaev@space307.com>
2020-11-12 13:12:05 +0300

    fix ast gendecl & fmt

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func main()  {
+func main() {
 	// директория проекта, в котором ищем структуры для генерации
 	dir := "."
 
@@ -19,7 +19,7 @@ func main()  {
 
 	//Проходимся по всем парам и генерим файлы с кодом преобразования
 	for from, to := range p.Connections {
-		err := generateFile(from,to)
+		err := generateFile(from, to)
 		if err != nil {
 			fmt.Printf("error: %s", err)
 			os.Exit(1)
